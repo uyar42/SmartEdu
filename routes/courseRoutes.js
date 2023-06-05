86ebb09c5df3ae89b13e0d1e9b4c7b099b7964f9
mehmet <71780605+uyar42@users.noreply.github.com>
2023-06-05 14:57:16 +0300

    Kurs Güncellemek

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -9,6 +9,7 @@ router
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
 router.route('/:slug').delete(courseController.deleteCourse);
+router.route('/:slug').put(courseController.updateCourse);
 router.route('/enroll').post(courseController.enrollCourse);
 router.route('/release').post(courseController.releaseCourse);
 
